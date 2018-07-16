297f60b0d3d91019718123740b65c1afbbe214c2
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-07-16 23:07:17 +0400

    Bug

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,24 @@
 package service;
 
-public class ProductService {
+import domain.Product;
+
+import java.util.List;
+
+/**
+ * Product service, it is wrapper for repository object.
+ */
+public interface ProductService {
+
+    /**
+     * Return all products.
+     * @return list of all products.
+     */
+    List<Product> getAllProducts();
+
+    /**
+     * Return a list of product which have given category.
+     * @param category string of searchable category.
+     * @return list of product which related to the category.
+     */
+    List<Product> getProductsByCategory(String category);
 }
--- src/main/java/service/ProductServiceImpl.java
+++ src/main/java/service/ProductServiceImpl.java
@@ -1,4 +1,45 @@
 package service;
 
-public class ProductServiceImpl {
+import domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    /**
+     * Product repository for execute some operations
+     */
+    private ProductRepository productRepo;
+
+    /**
+     * Create a new service for product operation.
+     * @param repository implemnetation of product repository interface.
+     */
+    @Autowired
+    public ProductServiceImpl(ProductRepository repository) {
+        this.productRepo = repository;
+    }
+
+    /**
+     * Return all products from the repository.
+     * @return list of all products.
+     */
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepo.getAllProducts();
+    }
+
+    /**
+     * Return products with given category.
+     * @param category string of searchable category.
+     * @return list of all products which have category is equals given category.
+     */
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return this.productRepo.getProductsByCategory(category);
+    }
 }
