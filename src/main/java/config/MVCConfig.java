1099e9e7c941988341f786cf209d5044f003a054
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-07-23 22:17:46 +0400

    Add a new configuration class which enable web mvc by sprin annotation.
    Add common multi part resolver for uploading file from client, and
    configure it by set max upload size of loading file.

--- src/main/java/config/MVCConfig.java
+++ src/main/java/config/MVCConfig.java
@@ -7,18 +7,36 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Config class, enable MVC of Spring by Enable MVC annotations.
+ * Configure static resources such as images, pdf and others.
+ * Also add bean for uploading a new file.
+ * @author vrnsky.
+ * @version 0.1
+ */
 @Configuration
 @EnableWebMvc
 public class MVCConfig implements WebMvcConfigurer {
+
+    /**
+     * Register a static resources which hold resources folder at the webapp.
+     * @param registry spring register.
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**").addResourceLocations("/resources/images/");
+        registry.addResourceHandler("/manuals/**").addResourceLocations("/resources/manuals/");
     }
 
+    /**
+     * Add bean for uploading file to the server.
+     * @return mulipart resolver which allow to work with files.
+     */
     @Bean
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("utf-8");
+        resolver.setMaxUploadSize(10240000);
         return resolver;
     }
 }
